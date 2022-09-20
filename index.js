747b3546675d6dcd9e156dc9226037145893b179
Cesar Hernandez <chema27281@gmail.com>
2022-09-19 22:12:09 -0400

    commit tree f5170474da71b14e9e7faccfa2df5d0cf9f53887

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function  distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue -42)
+}
+function distanceFromHqInFeet(someValue){
+    const distance = someValue;
+    return 264 * distanceFromHqInBlocks(distance);
+}
+function distanceTravelledInFeet(start,end){
+    return Math.abs(start-end) * 264;
+}
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end)
+    if (distance<= 400){
+        return 0;
+    }
+    else if(distance > 400 && distance < 2000){
+        return 0.02 * (distance -400);
+
+     } else if (distance > 2000 && distance < 2500){
+        return 25
+     } else if (distance > 2500){
+        return "cannot travel that far"
+     }
+    }
